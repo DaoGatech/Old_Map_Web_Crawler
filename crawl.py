71dc0681f58a18c94dee944e17c872961d0630bb
DaoGatech <stephenhdao@gatech.edu>
2016-08-26 17:31:21 -0400

    :art: add find-the-last-page function

--- crawl.py
+++ crawl.py
@@ -9,8 +9,14 @@ import CONFIG
 def crawl():
     web = urllib.request.urlopen(CONFIG.CRAWL_MAIN_LINK)
     soup = BeautifulSoup(web.read(), 'lxml')
+    pages = soup.findAll('div', {'class': 'pages'})
+    last_page = pages[0].findAll('a')[8].contents[0]
+
+    for i in range(1,int(last_page)):
+        print(i)
+        
     photo_ids = []
-    for script in soup.find_all("script", {"src": False}):
+    for script in soup.find_all('script', {'src': False}):
         if script:
             values = re.findall(r'var photo_ids =\s*(.*?);', script.string, re.DOTALL | re.MULTILINE)
             if len(values) > 0:
@@ -19,6 +25,8 @@ def crawl():
 
     photo_ids = json.loads(photo_ids[0])
     for photo in photo_ids:
-        print(photo)
+        web = urllib.request.urlopen(CONFIG.PHOTO_LINK + photo)
+        soup = BeautifulSoup(web.read(), 'lxml')
+        mydivs = soup.findAll('div', {'class': 'stylelistrow'})
 
 crawl()
