f90444712c5e9870b864c75ca7ccee38801a943d
DaoGatech <stephenhdao@gatech.edu>
2016-08-27 00:03:32 -0400

    :fire: change long to lon

--- database.py
+++ database.py
@@ -3,14 +3,14 @@ from pymysql import MySQLError
 import CONFIG
 
 
-def insert_to_database(title, lat, long, url):
+def insert_to_database(title, lat, lon, url):
     conn = \
         pymysql.connect(host='CONFIG.SQL_HOST'
                         , user='CONFIG.USER', passwd='CONFIG.PASS',
                         db='CONFIG.DB_NAME')
     cur = conn.cursor()
     try:
-        cur.execute(CONFIG.INSERT_COMMAND + "'" + title + "','" + lat + "','" + long + "','" + url + "')")
+        cur.execute(CONFIG.INSERT_COMMAND + "'" + title + "','" + lat + "','" + lon + "','" + url + "')")
         conn.commit()
         print('Database Success')
     except MySQLError as e:
