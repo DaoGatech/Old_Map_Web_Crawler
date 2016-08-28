6fa1b6d71204326e61494342ca6670db0368c87b
DaoGatech <stephenhdao@gatech.edu>
2016-08-28 18:43:09 -0400

    :bug: Fix database problem

--- database.py
+++ database.py
@@ -4,14 +4,16 @@ import CONFIG
 
 
 def insert_to_database(title, lat, lon, url):
-    conn = \
-        pymysql.connect(host='CONFIG.SQL_HOST'
-                        , user='CONFIG.USER', passwd='CONFIG.PASS',
-                        db='CONFIG.DB_NAME')
-    cur = conn.cursor()
     try:
+        conn = \
+            pymysql.connect(host=CONFIG.SQL_HOST
+                            , user=CONFIG.USER, passwd=CONFIG.PASS,
+                            db=CONFIG.DB_NAME, charset='utf8')
+        cur = conn.cursor()
+        print(CONFIG.INSERT_COMMAND + "'" + title + "','" + lat + "','" + lon + "','" + url + "')")
         cur.execute(CONFIG.INSERT_COMMAND + "'" + title + "','" + lat + "','" + lon + "','" + url + "')")
         conn.commit()
         print('Database Success')
     except MySQLError as e:
+        print(e)
         pass
