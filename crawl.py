464e0201e93a7e50aa419a89ea42fb9e32817f15
DaoGatech <stephenhdao@gatech.edu>
2016-08-29 18:45:21 -0400

    :new: add comments for code

--- crawl.py
+++ crawl.py
@@ -6,11 +6,15 @@ import lxml
 import CONFIG
 import database
 
+
 def crawl():
+    # Crawl the page to figure out how many pages of images
     web = urllib.request.urlopen(CONFIG.CRAWL_MAIN_LINK)
     soup = BeautifulSoup(web.read(), 'lxml')
     pages = soup.findAll('div', {'class': 'pages'})
     last_page = pages[0].findAll('a')[len(pages[0].findAll('a')) - 2].contents[0]
+
+    # Crawl all the data of images in each page and figure list of photo ids
     for i in range(1, int(last_page)):
         web = urllib.request.urlopen(CONFIG.CRAWL_MAIN_LINK_WITH_PAGE + str(i))
         soup = BeautifulSoup(web.read(), 'lxml')
@@ -21,6 +25,8 @@ def crawl():
                 if len(values) > 0:
                     photo_ids = values
                     break
+
+        # Return the data of each image based on its photo id
         photo_ids = json.loads(photo_ids[0])
         for photo in photo_ids:
             web = urllib.request.urlopen(CONFIG.PHOTO_LINK + str(photo))
@@ -31,6 +37,7 @@ def crawl():
             latitude = latitude[0]['title']
             longitude = longitude[0]['title']
             img_url = CONFIG.IMG_SRC + str(photo) + '.jpg'
+            # Store the data into the database
             database.insert_to_database(img_title, latitude, longitude, img_url)
             print(str(photo) + ' = Success!')
 crawl()
